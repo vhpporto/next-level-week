9a1ea6ad822b8c1547dd2c46046cfdb04c92de84
Vitor Porto <vhpporto@gmail.com>
2020-06-02 16:54:50 -0700

    upd

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,8 +1,32 @@
 async function getUfs() {
-  const ufSelect = document.querySelector("select[name=uf");
-  fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+  const ufSelect = document.querySelector("select[name=uf]");
+  await fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
     .then((response) => response.json())
-    .then((responseJson) => console.log(responseJson));
+    .then((states) => {
+      for (const state of states) {
+        ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
+      }
+    });
 }
 
 getUfs();
+
+async function getCities(event) {
+  const citySelected = document.querySelector("select[name=city]");
+  const ufValue = event.target.value;
+  citySelected.innerHTML = "<option>Selecione a Cidade</option>";
+  console.log(ufValue);
+  const url = `
+  https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
+  await fetch(url)
+    .then((response) => response.json())
+    .then((cities) => {
+      for (const city of cities) {
+        console.log(city.nome);
+        citySelected.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
+      }
+      citySelected.disabled = false;
+    });
+}
+
+document.querySelector("select[name=uf]").addEventListener("change", getCities);
