d32c36bc85f3541716547f6367daa1f3c7ef23f4
Vitor <suporte@MacBook-Pro-de-Vitor.local>
2020-06-03 17:08:53 -0300

    upd

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -15,6 +15,7 @@ async function getCities(event) {
   const citySelected = document.querySelector("select[name=city]");
   const ufValue = event.target.value;
   citySelected.innerHTML = "<option>Selecione a Cidade</option>";
+  citySelected.disabled = true;
   console.log(ufValue);
   const url = `
   https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
@@ -23,10 +24,40 @@ async function getCities(event) {
     .then((cities) => {
       for (const city of cities) {
         console.log(city.nome);
-        citySelected.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
+        citySelected.innerHTML += `<option value="${city.nome}">${city.nome}</option>`;
       }
       citySelected.disabled = false;
     });
 }
 
 document.querySelector("select[name=uf]").addEventListener("change", getCities);
+
+const itemsToCollect = document.querySelectorAll(".items-grid li");
+
+for (const item of itemsToCollect) {
+  item.addEventListener("click", handleSelectedItem);
+}
+
+let collectedItems = document.querySelector("input[name=items]");
+
+let selectedItems = [];
+
+function handleSelectedItem(event) {
+  const itemId = event.target.dataset.id;
+  const itemLi = event.target;
+
+  itemLi.classList.toggle("selected");
+
+  const alreadySelected = selectedItems.findIndex((item) => item == itemId);
+
+  if (alreadySelected >= 0) {
+    const filteredItems = selectedItems.filter((item) => {
+      const itemIsDifferent = item != itemId;
+      return itemIsDifferent;
+    });
+    selectedItems = filteredItems;
+  } else {
+    selectedItems.push(itemId);
+  }
+  collectedItems = selectedItems;
+}
---
+++ scripts/home.js
@@ -0,0 +1,8 @@
+const showModal = document.querySelector(".hide");
+
+function closeModal() {
+  showModal.classList.toggle("hide");
+  console.log(showModal);
+}
+
+closeModal();
